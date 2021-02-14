a535b1b91f34965aae7138bd0d43d05bd1fec607
reybos <andreybosiy@yandex.ru>
2021-02-14 20:47:54 +0300

    1588. Sum of All Odd Length Subarrays

--- src/main/java/sum/of/all/odd/length/subarrays/Solution.java
+++ src/main/java/sum/of/all/odd/length/subarrays/Solution.java
@@ -53,6 +53,14 @@ public class Solution {
             rsl += sum;
         }
         return rsl;
+        /*
+        this is not my solution (found on leetcode) but very sorry
+        int res = 0, n = arr.length;
+        for (int i = 0; i < n; ++i) {
+            res += ((i + 1) * (n - i) + 1) / 2 * arr[i];
+        }
+        return res;
+         */
     }
 
     private int[] getSumIfEven(int[] arr) {
